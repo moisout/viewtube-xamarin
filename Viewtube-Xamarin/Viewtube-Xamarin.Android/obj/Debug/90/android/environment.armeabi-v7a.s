3cd921bea704478286ebe9ef553dc3c6e617980b
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2020-02-05 17:39:49 +0100

    player things

--- Viewtube-Xamarin/Viewtube-Xamarin.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ Viewtube-Xamarin/Viewtube-Xamarin.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"c672372f-1db8-40bb-9fa6-c06063e75da5"
+	.asciz	"554a0f21-afc7-45ac-9ac4-5708318b1a07"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
